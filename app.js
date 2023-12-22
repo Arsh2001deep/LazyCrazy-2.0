66ff08a61bc636d22d1693b65cab598d8208c971
Arsh2001deep <arshdeep.singh.fiem.cse20@teamfuture.in>
2023-12-22 14:57:57 +0530

    added theme

--- app.js
+++ app.js
@@ -9,4 +9,26 @@ ham.addEventListener("click", () => {
 
 })
 
-// aos
+// added by me
+
+
+document.addEventListener('DOMContentLoaded', function() {
+    const themeToggle = document.getElementById('theme-toggle');
+    const body = document.body;
+
+
+
+
+    // Check if user has a preferred theme
+    const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
+
+    // Set initial theme based on user preference
+    if (prefersDarkMode) {
+        body.classList.add('dark-mode');
+    }
+
+    // Toggle theme on button click
+    themeToggle.addEventListener('click', function() {
+        body.classList.toggle('dark-mode');
+    });
+});
