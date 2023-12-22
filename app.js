af0798938e90b2fa96e3937428e02f370abed057
Arsh2001deep <arshdeep.singh.fiem.cse20@teamfuture.in>
2023-12-22 16:58:38 +0530

    final commit

--- app.js
+++ app.js
@@ -1,13 +1,21 @@
-console.log("object");
-
-const ham = document.querySelector("#ham")
-const menu = document.querySelector("#menu")
+// menu
+const ham = document.querySelector("#ham");
+const menu = document.querySelector("#menu");
 
 ham.addEventListener("click", () => {
-    menu.classList.toggle("hidden")
+    menu.classList.toggle("hidden");
+});
 
+// Listen for clicks on the document
+document.addEventListener("click", (event) => {
+    const isClickInsideMenu = menu.contains(event.target);
+    const isClickOnHam = ham.contains(event.target);
 
-})
+    // If the click is outside the menu and not on the ham element, hide the menu
+    if (!isClickInsideMenu && !isClickOnHam) {
+        menu.classList.add("hidden");
+    }
+});
 
 // added by me
 
@@ -22,13 +30,31 @@ document.addEventListener('DOMContentLoaded', function() {
     // Check if user has a preferred theme
     const prefersDarkMode = window.matchMedia('(prefers-color-scheme: dark)').matches;
 
-    // Set initial theme based on user preference
-    if (prefersDarkMode) {
-        body.classList.add('dark-mode');
-    }
-
     // Toggle theme on button click
     themeToggle.addEventListener('click', function() {
         body.classList.toggle('dark-mode');
+    });
+});
+
+
+
+
+// scroll
+document.addEventListener('DOMContentLoaded', function() {
+    // Smooth scrolling for anchor links
+    document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+        anchor.addEventListener('click', function(e) {
+            e.preventDefault();
+
+            const targetId = this.getAttribute('href').substring(1);
+            const targetElement = document.getElementById(targetId);
+
+            if (targetElement) {
+                window.scrollTo({
+                    top: targetElement.offsetTop,
+                    behavior: 'smooth'
+                });
+            }
+        });
     });
 });
